e0ace793196437eb25a11892cb4bb02afe28f69d
Simon Adorf <simon.adorf@epfl.ch>
2020-08-11 16:44:11 +0000

    Revert changes to requirements.

--- install_dev.sh
+++ install_dev.sh
@@ -1,5 +1,5 @@
 # Used this command on 2019-04-25
-pip install aiida-core[rest,atomic_tools]==1.0.0b2 git+https://github.com/aiidateam/aiida-quantumespresso@e5e1a4b47e7d19c59af8174157d7eb65a3b9292d#egg=aiida_quantumespresso aiida-zeopp==1.0.0a2 aiida-qeq==1.0.0a1 aiida-diff==1.0.0a1 aiida-cp2k==1.0.0b1 appmode-aiidalab bokeh bqplot cookiecutter dulwich fileupload html5lib ipympl "ipython<6.0" lxml nglview pythreejs requests-cache "tornado<5"
+pip install aiida-core[rest,atomic_tools]==1.0.0b2 git+https://github.com/aiidateam/aiida-quantumespresso@e5e1a4b47e7d19c59af8174157d7eb65a3b9292d#egg=aiida_quantumespresso aiida-zeopp==1.0.0a2 aiida-qeq==1.0.0a1 aiida-diff==1.0.0a1 aiida-cp2k==1.0.0b1 appmode-aiidalab bokeh bqplot cookiecutter dulwich fileupload html5lib ipympl "ipython<6.0" lxml nglview pythreejs requests-cache "tornado<5" markdown
 
 # (may have to remove aiidalab dependency)
 pip freeze > requirements.txt
