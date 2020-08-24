9af0550e00cb55ed248f2aa792bf150120198320
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-08-24 11:19:34 +0200

    Fix apidoc build.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,9 @@
 
 # -*- coding: utf-8 -*-
 """Sphinx configuration for aiidalab."""
-
+import os
+import subprocess
+import sys
 import time
 import aiidalab
 
@@ -71,6 +73,46 @@ htmlhelp_basename = 'aiidalab-doc'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+
+def run_apidoc(_):
+    """Runs sphinx-apidoc when building the documentation.
+    Needs to be done in conf.py in order to include the APIdoc in the
+    build on readthedocs.
+    See also https://github.com/rtfd/readthedocs.org/issues/1139
+    """
+    source_dir = os.path.abspath(os.path.dirname(__file__))
+    apidoc_dir = os.path.join(source_dir, 'apidoc')
+    package_dir = os.path.join(source_dir, os.pardir, os.pardir, 'aiidalab')
+
+    # In #1139, they suggest the route below, but this ended up
+    # calling sphinx-build, not sphinx-apidoc
+    #from sphinx.apidoc import main
+    #main([None, '-e', '-o', apidoc_dir, package_dir, '--force'])
+
+    cmd_path = 'sphinx-apidoc'
+    if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
+        # If we are, assemble the path manually
+        cmd_path = os.path.abspath(os.path.join(sys.prefix, 'bin', 'sphinx-apidoc'))
+
+    options = [
+        '-o',
+        apidoc_dir,
+        package_dir,
+        '--private',
+        '--force',
+        '--no-toc',
+    ]
+
+    # See https://stackoverflow.com/a/30144019
+    env = os.environ.copy()
+    env["SPHINX_APIDOC_OPTIONS"] = 'members,special-members,private-members,undoc-members,show-inheritance'
+    subprocess.check_call([cmd_path] + options, env=env)
+
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
+
 
 # Warnings to ignore when using the -n (nitpicky) option
 # We should ignore any python built-in exception, for instance
