d558cb281ec505b1057036b08987316a17e52e08
Simon Adorf <simon.adorf@epfl.ch>
2020-09-02 07:46:46 +0000

    Implement unit tests for app installation and uninstallation.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 """Tests for the aiidalab.app module."""
-# pylint: disable=unused-argument,redefined-outer-name
+# pylint: disable=unused-argument,redefined-outer-name,invalid-name,protected-access
 import json
 from pathlib import Path
 from subprocess import run, CalledProcessError
@@ -9,6 +9,10 @@ import pytest
 
 import aiidalab
 from aiidalab.app import AiidaLabApp
+from aiidalab.config import AIIDALAB_DEFAULT_GIT_BRANCH as DEFAULT_BRANCH
+
+HELLO_WORLD_APP_DIR = Path(__file__).parent.joinpath('data', 'aiidalab-hello-world').resolve()
+TESTING_BRANCH = 'testing-3926140692'  # we expect that this branch does not exist
 
 
 def clone(source, target):
@@ -19,45 +23,6 @@ def clone(source, target):
 @pytest.fixture
 def empty_registry():
     return dict(apps=dict(), categories=dict())
-
-
-HELLO_WORLD_APP_DIR = Path(__file__).parent.joinpath('data', 'aiidalab-hello-world').resolve()
-
-
-@pytest.fixture
-def hello_world_app(environment):
-    """Return a AiidaLabApp fixture based on the hello-world-app."""
-    # Clone the hello-world-app repository into a local path that will
-    # serve as the "remote" origin.
-    origin = Path(aiidalab.config.AIIDALAB_HOME).joinpath('local', 'aiidalab-hello-world').resolve()
-    origin.parent.mkdir()
-    try:
-        clone(source=str(HELLO_WORLD_APP_DIR), target=str(origin))
-    except CalledProcessError as error:
-        pytest.skip(f"missing submodules to run this test: {error}")
-
-    # Clone the app from origin into the apps/ folder.
-    apps_path = Path(aiidalab.config.AIIDALAB_APPS)
-    app_path = apps_path.joinpath('hello-world').resolve()
-    clone(source=str(origin), target=str(app_path))
-
-    # Register app in app registry
-    app_data = json.loads(app_path.joinpath('metadata.json').read_text())
-    app_registry_data = {
-        "git_url": str(origin),
-        "meta_url": "https://raw.githubusercontent.com/aiidalab/aiidalab-hello-world/master/metadata.json",
-        "categories": ["Utilities"],
-        "groups": ["utilities"],
-        "metainfo": app_data
-    }
-    assert aiidalab.config.AIIDALAB_REGISTRY.startswith('file://')
-    registry_path = Path(aiidalab.config.AIIDALAB_REGISTRY[len('file://'):])
-    registry = json.loads(registry_path.read_text())
-    assert 'hello-world' not in registry['apps']
-    registry['apps']['hello-world'] = app_registry_data
-
-    # Finally, yield AiidaLabApp
-    yield AiidaLabApp('hello-world', app_registry_data, apps_path, watch=False)
 
 
 @pytest.fixture
@@ -74,7 +39,61 @@ def environment(tmp_path, monkeypatch, empty_registry):
     Path(aiidalab.config.AIIDALAB_HOME).mkdir()
     Path(aiidalab.config.AIIDALAB_APPS).mkdir()
     registry_path.write_text(json.dumps(empty_registry) + '\n')
+
+    monkeypatch.setattr(aiidalab.app.AiidaLabApp, 'refresh_async', aiidalab.app.AiidaLabApp.refresh)
+
     yield
+
+
+@pytest.fixture
+def _hello_world_app_remote_origin(environment):
+    """Create and configure a temporary remote origin for the hello world app."""
+    # Clone the hello-world-app repository into a local path that will
+    # serve as the "remote" origin.
+    origin = Path(aiidalab.config.AIIDALAB_HOME).joinpath('local', 'aiidalab-hello-world').resolve()
+    origin.parent.mkdir()
+    try:
+        clone(source=str(HELLO_WORLD_APP_DIR), target=str(origin))
+    except CalledProcessError as error:
+        pytest.skip(f"missing submodules to run this test: {error}")
+    else:
+        run([
+            'git', '-c', 'user.name=pytest', '-c', 'user.email=pytest@example.com', 'commit', '-m', 'latest',
+            '--allow-empty'
+        ],
+            cwd=str(origin),
+            check=True)
+        run(['git', 'branch', TESTING_BRANCH], cwd=str(origin), check=True)
+        run(['git', 'tag', 'test-tag', 'HEAD~~'], cwd=str(origin), check=True)
+    yield origin
+
+
+def _register_hello_world_app(url):
+    """Register the hello world app with the given url in the app registry."""
+    app_data = json.loads(HELLO_WORLD_APP_DIR.joinpath('metadata.json').read_text())
+    app_registry_data = {
+        "git_url": url,
+        "meta_url": "https://raw.githubusercontent.com/aiidalab/aiidalab-hello-world/master/metadata.json",
+        "categories": ["Utilities"],
+        "groups": ["utilities"],
+        "metainfo": app_data
+    }
+    assert aiidalab.config.AIIDALAB_REGISTRY.startswith('file://')
+    registry_path = Path(aiidalab.config.AIIDALAB_REGISTRY[len('file://'):])
+    registry = json.loads(registry_path.read_text())
+    assert 'hello-world' not in registry['apps']
+    registry['apps']['hello-world'] = app_registry_data
+    registry_path.write_text(json.dumps(registry))
+    return app_registry_data
+
+
+@pytest.fixture(params=['', f'@{DEFAULT_BRANCH}', f'@{TESTING_BRANCH}'])
+def hello_world_app(_hello_world_app_remote_origin, request):
+    """Return a AiidaLabApp fixture based on the hello-world-app."""
+    url = f"{_hello_world_app_remote_origin}{request.param}"
+    app_registry_data = _register_hello_world_app(url)
+    apps_path = Path(aiidalab.config.AIIDALAB_APPS)
+    yield AiidaLabApp('hello-world', app_registry_data, apps_path, watch=False)
 
 
 def test_environment_configuration(environment):
@@ -90,10 +109,24 @@ def test_hello_world_app_setup(hello_world_app):
     """Check the basic function of the AiidaLabApp class."""
     app = hello_world_app
     assert app.name == 'hello-world'
-    assert Path(app.path).is_dir()
-    assert app.is_installed()
+    assert not app.is_installed()
     for key in 'authors', 'description', 'title':
         assert key in app.metadata
         assert getattr(app, key) == app.metadata[key]
     for key in 'logo', 'state', 'version':
         assert key in app.metadata
+
+
+def test_hello_world_app_install_uninstall(hello_world_app):
+    """Test that the app can be uninstalled and installed again."""
+    app = hello_world_app
+    assert not app.is_installed()
+    app.install_app()
+    assert Path(app.path).is_dir()
+    assert app.is_installed()
+    assert app.installed_version is not aiidalab.app.AppVersion.NOT_INSTALLED
+    assert app._release_line.line in app.installed_version
+    app.uninstall_app()
+    assert not app.is_installed()
+    assert not Path(app.path).exists()
+    assert app.installed_version is aiidalab.app.AppVersion.NOT_INSTALLED
