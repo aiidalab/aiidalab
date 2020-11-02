5d18d808a799edf1096755beff03dfc7628c71d3
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-11-02 14:11:59 +0100

    Remove if __name__... from setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,20 @@ import json
 
 from setuptools import setup, find_packages
 
-if __name__ in ["__main__", "builtins"]:
+with open('setup.json', 'r') as info:
+    kwargs = json.load(info)  # pylint: disable=invalid-name
 
-    with open('setup.json', 'r') as info:
-        kwargs = json.load(info)  # pylint: disable=invalid-name
-
-    setup(
-        packages=find_packages(),
-        include_package_data=True,
-        reentry_register=True,
-        long_description=open('README.md').read(),
-        long_description_content_type='text/markdown',
-        data_files=[
-            # like `jupyter nbextension enable --sys-prefix`
-            ("etc/jupyter/nbconfig/notebook.d", ["jupyter-config/nbconfig/notebook.d/aiidalab.json"]),
-            # like `jupyter serverextension enable --sys-prefix`
-            ("etc/jupyter/jupyter_notebook_config.d", ["jupyter-config/jupyter_notebook_config.d/aiidalab.json"])
-        ],
-        zip_safe=False,
-        **kwargs)
+setup(
+    packages=find_packages(),
+    include_package_data=True,
+    reentry_register=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    data_files=[
+        # like `jupyter nbextension enable --sys-prefix`
+        ("etc/jupyter/nbconfig/notebook.d", ["jupyter-config/nbconfig/notebook.d/aiidalab.json"]),
+        # like `jupyter serverextension enable --sys-prefix`
+        ("etc/jupyter/jupyter_notebook_config.d", ["jupyter-config/jupyter_notebook_config.d/aiidalab.json"])
+    ],
+    zip_safe=False,
+    **kwargs)
