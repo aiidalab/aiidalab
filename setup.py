e7bdde8e0cb6607e6442f1bb39c6efb9d5a0af86
Chris Sewell <chrisj_sewell@hotmail.com>
2021-03-22 17:46:09 +0100

    move setup configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
 # -*- coding: utf8 -*-
-"""Setting up base widgets for base package for AiiDAlab."""
-import json
+"""This file is required for editable installs of the package."""
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-with open("setup.json", "r") as info:
-    kwargs = json.load(info)  # pylint: disable=invalid-name
-
-setup(
-    packages=find_packages(),
-    include_package_data=True,
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    data_files=[
-        # like `jupyter nbextension enable --sys-prefix`
-        (
-            "etc/jupyter/nbconfig/notebook.d",
-            ["jupyter-config/nbconfig/notebook.d/aiidalab.json"],
-        ),
-        # like `jupyter serverextension enable --sys-prefix`
-        (
-            "etc/jupyter/jupyter_notebook_config.d",
-            ["jupyter-config/jupyter_notebook_config.d/aiidalab.json"],
-        ),
-    ],
-    zip_safe=False,
-    **kwargs
-)
+setup()
