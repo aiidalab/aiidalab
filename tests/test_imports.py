688264057b37266b83911fc0cd1ae4a672b2cb4d
Carl Simon Adorf <simon.adorf@epfl.ch>
2021-05-12 17:16:06 +0200

    Use revised registry (#174)
    * Revise AiiDAlab to work with the revised app registry index.
    * Refactor the app module to separate core app management functions
    and traits.
    * Replace app tests with smoke tests.
    * Fix issue with determining updates for incompatible apps.
    * Update the default registry endpoint.
    * Fix: Set reasons for incompatibility in `compatibility_info` trait.

---
+++ tests/test_imports.py
@@ -0,0 +1,11 @@
+def test_imports():
+    """Test that most important modules can be imported
+
+    This is just a basic smoke test.
+    """
+    # flake8: noqa: F401
+
+    import aiidalab
+    import aiidalab.app
+    import aiidalab.config
+    import aiidalab.utils
