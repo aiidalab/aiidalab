fc0e8104e413df507b4c301e379ea3632a2701e3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 08:42:32 +0000

    Adding tarfile member sanitization to extractall()

--- aiidalab/app.py
+++ aiidalab/app.py
@@ -323,7 +323,26 @@ class _AiidaLabApp:
         else:
             with tempfile.TemporaryDirectory() as tmp_dir:
                 with tarfile.open(path) as tar_file:
-                    tar_file.extractall(path=tmp_dir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar_file, path=tmp_dir)
                     self._install_from_path(Path(tmp_dir))
 
     def _install_from_https(self, url):
