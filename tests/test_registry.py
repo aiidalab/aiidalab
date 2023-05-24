8f3362239d9736c2b9d40b2f11919c51b2aee01f
Jusong Yu <jusong.yeu@gmail.com>
2023-05-25 00:37:54 +0200

    f-d

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,5 +1,4 @@
 import os
-from urllib.parse import urlsplit
 
 from aiidalab.fetch import GitRepo, fetch_from_url
 from aiidalab.registry.releases import _get_release_commits, _split_release_line
@@ -9,12 +8,10 @@ def test_get_all_tagged_releases():
     """Test that all tagged releases are returned."""
     url = "git+https://github.com/aiidalab/aiidalab-qe.git@*:v23.04.0^.."
     base_url, release_line = _split_release_line(url)
-    parsed_url = urlsplit(base_url)
 
     with fetch_from_url(base_url) as repo_path:
-        if parsed_url.scheme.startswith("git+"):
-            repo = GitRepo(os.fspath(repo_path))
-            releases = [tag for tag, _ in _get_release_commits(repo, release_line)]
+        repo = GitRepo(os.fspath(repo_path))
+        releases = [tag for tag, _ in _get_release_commits(repo, release_line)]
 
     assert "v23.04.2" in releases
 
@@ -23,12 +20,10 @@ def test_get_releases_from_branch():
     """Test that all tagged releases of perticular branch (main) are returned."""
     url = "git+https://github.com/aiidalab/aiidalab-qe.git@main:v23.04.0^.."
     base_url, release_line = _split_release_line(url)
-    parsed_url = urlsplit(base_url)
 
     with fetch_from_url(base_url) as repo_path:
-        if parsed_url.scheme.startswith("git+"):
-            repo = GitRepo(os.fspath(repo_path))
-            releases = [tag for tag, _ in _get_release_commits(repo, release_line)]
+        repo = GitRepo(os.fspath(repo_path))
+        releases = [tag for tag, _ in _get_release_commits(repo, release_line)]
 
     assert "v23.04.2" not in releases
     assert "v23.04.0" in releases
