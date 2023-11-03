35c63f94ca2e9ba1c4f384e6af9c4289fbc33985
Jusong Yu <jusong.yeu@gmail.com>
2023-11-03 16:07:10 +0100

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "aiidalab"
-copyright_first_year = "2023"
+copyright_first_year = "2020"
 copyright_owners = "The AiiDAlab Team"
 
 current_year = str(time.localtime().tm_year)
