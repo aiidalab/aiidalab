ee03fa600a2b7ed39357ff64fc664bd46f03ef39
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-08-14 11:57:48 +0100

    Skip invalid versions in _AiidaLabApp.releases

--- aiidalab/utils.py
+++ aiidalab/utils.py
@@ -13,7 +13,7 @@ from functools import wraps
 from pathlib import Path
 from subprocess import run
 from threading import Lock
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Iterable
 from urllib.parse import urlsplit, urlunsplit
 
 import requests
@@ -212,6 +212,33 @@ def get_package_by_name(packages: dict[str, Package], name: str) -> Package | No
         if package.canonical_name == canonicalize_name(name):
             return package
     return None
+
+
+def is_valid_version(version: str) -> bool:
+    """Return True if given string is a PEP440-compliant version, False otherwise."""
+    from packaging.version import InvalidVersion, parse
+
+    try:
+        parse(version)
+    except InvalidVersion:
+        return False
+    else:
+        return True
+
+
+def sort_semantic(versions: Iterable[str], prereleases: bool = False) -> list[str]:
+    """Sort app versions semantically according to semver, latest first.
+    Optionally filter out the pre-release versions.
+
+    :raises: packaging.version.InvalidVersion if the input list contains invalid version.
+    """
+    from packaging.version import parse
+
+    return [
+        version
+        for version in sorted(versions, key=parse, reverse=True)
+        if prereleases or not parse(version).is_prerelease
+    ]
 
 
 def split_git_url(git_url: str) -> tuple[str, str | None]:
